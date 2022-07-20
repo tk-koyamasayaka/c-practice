40f585e693abc060a6ea51536761d151c4167fec
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-20 23:33:22 +0900

    %桁数d

--- 20220722/main.c
+++ 20220722/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-  int value;
-  value = 10;
-  printf("%d",value);
+  int a = 1000, b = 500, c = 3;
+
+  printf("%5d ¥n", a);
+  printf("%5d ¥n", b);
+  printf("%5d ¥n", c);
+
   return 0;
 }
